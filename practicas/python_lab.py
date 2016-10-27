6a860d28d5f05f1680392d5fbee80715fbf524f0
kchaires <kdchaires@gmail.com>
2016-10-27 18:09:55 +0000

    combinar

--- practicas/python_lab.py
+++ practicas/python_lab.py
@@ -34,7 +34,7 @@ def divisible_by_3(num):
     >>> divisible_by_3(7)
     False
     """
-    return "True" if num % 3 == 0 else "False"
+    return True if num % 3 == 0 else False
 
 
 def predict_expresion(x, y, prediction):
